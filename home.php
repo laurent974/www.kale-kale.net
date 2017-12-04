17deac773833c1fda9a13b3c3b5e9dc48bd2fcec
Yen-Pon Laurent <lyenpon974@gmail.com>
2017-12-04 13:50:07 +0400

    Add - favicon + Logo

--- home.php
+++ home.php
@@ -12,8 +12,12 @@
   	<![endif]-->
 
     <!-- Favicon -->
-    <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/assets/img/favicon.ico" type="image/x-icon">
-    <link rel="icon" href="<?php echo get_template_directory_uri(); ?>/assets/img/favicon.ico" type="image/x-icon">
+    <link rel="apple-touch-icon" sizes="76x76" href="<?php echo get_template_directory_uri(); ?>/assets/img/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri(); ?>/assets/img/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_template_directory_uri(); ?>/assets/img/favicon-16x16.png">
+    <link rel="manifest" href="<?php echo get_template_directory_uri(); ?>/assets/img/manifest.json">
+    <link rel="mask-icon" href="<?php echo get_template_directory_uri(); ?>/assets/img/safari-pinned-tab.svg" color="#fff">
+    <meta name="theme-color" content="#ffffff">
 
   	<?php wp_head(); ?>
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
@@ -23,45 +27,49 @@
   </head>
   <body>
     <div class="Global"><!-- .Global -->
-      <nav class="navbar navbar-default navbar-fixed-top" role="navigation"><!-- .navbar -->
-        <div class="navbar-inner container-fluid"><!-- .container-fluid -->
+      <div class="Header container-fluid">
+        <nav class="navbar navbar-default navbar-fixed-top" role="navigation"><!-- .navbar -->
+          <div class="navbar-inner container-fluid"><!-- .container-fluid -->
 
-          <div class="navbar-header"><!-- .navbar-header -->
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#menu-menu-1"><!-- .navbar-toggle -->
-              <span class="sr-only">Toggle navigation</span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-            </button><!-- /.navbar-toggle -->
+            <div class="navbar-header"><!-- .navbar-header -->
+              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#menu-header-menu"><!-- .navbar-toggle -->
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button><!-- /.navbar-toggle -->
 
-            <div class="navbar-Lang-Mobile hidden-md"><!-- .navbar-Lang -->
-              <?php do_action('wpml_add_language_selector'); ?>
-            </div><!-- /.navbar-Lang -->
+              <div class="navbar-Lang-Mobile hidden-md"><!-- .navbar-Lang -->
+                <?php do_action('wpml_add_language_selector'); ?>
+              </div><!-- /.navbar-Lang -->
 
-            <a class="navbar-brand" href="<?php bloginfo('url')?>">
-              <img class="Logo" src="<?php echo get_template_directory_uri(); ?>/assets/img/logo.png" alt="Kalé Kalé" width="187" height="95">
-            </a>
-          </div><!-- /!.navbar-header -->
+              <a class="navbar-brand" href="<?php bloginfo('url')?>">
+                <img class="Logo" src="<?php echo get_template_directory_uri(); ?>/assets/img/logo.svg" alt="Kalé Kalé" width="187" height="95">
+              </a>
+            </div><!-- /!.navbar-header -->
             <?php
-              wp_nav_menu( array(
-                'menu'              => 'header-menu',
-                'depth'             => 0,
-                'container'         => false,
-                'menu_class'        => 'nav navbar-nav',
-                'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
-                'walker'            => new WP_Bootstrap_Navwalker())
-              );
+            wp_nav_menu( array(
+              'menu'              => 'header-menu',
+              'depth'             => 0,
+              'container'         => false,
+              'menu_class'        => 'nav navbar-nav collapse',
+              'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
+              'walker'            => new WP_Bootstrap_Navwalker())
+            );
             ?>
 
             <div class="navbar-Lang pull-right hidden-xs"><!-- .navbar-Lang -->
               <?php do_action('wpml_add_language_selector'); ?>
             </div><!-- /.navbar-Lang -->
           </div><!-- /!.container-fluid -->
-      </nav><!-- /!.navbar -->
+        </nav><!-- /!.navbar -->
+      </div>
 
-      <main class="Main"><!-- .Main --->
+      <main class="Main container-fluid"><!-- .Main --->
         Home
       </main><!-- /.Main -->
+
+      <?php wp_footer(); ?>
     </div><!-- /.Global -->
   </body>
 </html>
